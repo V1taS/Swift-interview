3d785eb99b35507dd2621941e70962769135cf48
Sosin Vitalii <375693@mail.ru>
2021-02-20 18:10:16 +0300

    Initial Commit

---
+++ Swift interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift interview
+//
+//  Created by Vitalii Sosin on 20.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
