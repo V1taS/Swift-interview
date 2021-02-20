f1969a25d241e9f93c38ce092e76ea64f0599b1b
Sosin Vitalii <375693@mail.ru>
2021-02-20 18:31:24 +0300

    first commit

--- Swift interview/System/SceneDelegate.swift
+++ Swift interview/System/SceneDelegate.swift
@@ -5,17 +5,20 @@
 //  Created by Vitalii Sosin on 20.02.2021.
 //
 
-import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.overrideUserInterfaceStyle = .light
+            window.rootViewController = UIHostingController(rootView: MainView())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
